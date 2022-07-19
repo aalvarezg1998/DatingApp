f028c57862d39c0d2fb598bfc26e963b75c63565
aalvarezg29 <ing.alexanderg1998@gmail.com>
2022-07-19 14:09:30 -0500

    Seccion 4

---
+++ API/services/TokenServices.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using API.Entities;
+using API.interfaces;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.services
+{
+    public class TokenServices : ITokenService
+    {
+        private readonly SymmetricSecurityKey _key;
+        public TokenServices(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
+
+        public string CreateToken(AppUser appUser)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, appUser.UserName)
+            };
+
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            var tokeHandler = new JwtSecurityTokenHandler();
+
+            var token = tokeHandler.CreateToken(tokenDescriptor);
+
+            return tokeHandler.WriteToken(token); 
+        }
+    }
+}
